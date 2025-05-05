7c7a0a44eaebb1ee9cb68a36eb04238dd8d90332
Sreemanth23 <sreemanth.b123@gmail.com>
2025-05-05 09:11:10 +0530

    ver_1.0.1

--- docs/application.0e52c.js
+++ docs/application.0e52c.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.4b0d3.json';
+          this.settingsPath = 'src/settings.17a0d.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- docs/index.38362.js
+++ docs/index.38362.js
@@ -1,4 +1,4 @@
-System.register(["./application.35d7b.js"], function (_export, _context) {
+System.register(["./application.0e52c.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
